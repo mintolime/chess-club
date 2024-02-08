0d2da3a25fcbe52fc95ebb1b14ed645326fc02f6
mintolime <104442896+mintolime@users.noreply.github.com>
2024-02-08 05:52:12 +0000

    fix: fixed slider for cards

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,68 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".participants__lists");
+  const containerParticipants = document.querySelector(".participants__lists");
   const cards = document.querySelectorAll(".participants__list");
+  const totalCards = document.querySelectorAll(".participants__list").length;
   const prevButton = document.querySelector("#rigth");
   const nextButton = document.querySelector("#left");
 
   let currentIndex = 0;
-
+  let firstCardIndex = 1;
   const disableBtn = (currentCount,totalCount,btn) => currentCount === totalCount ? btn.disabled = true : btn.disabled = false
 
   function updateUI(data) {
+    const currentCount = currentIndex + 1
+    const countOutput = document.querySelector(".participants__count");
+    countOutput.textContent = `${currentCount}/${totalCards}`; 
+    // disableBtn(currentCount,totalCards,nextButton)
+    disableBtn(currentCount,firstCardIndex,prevButton)
+
     if(data){
-      const totalCards = data.length;
-      const currentCount = currentIndex + 1
-      const countOutput = document.querySelector(".participants__count");
-      countOutput.textContent = `${currentCount}/${totalCards}`; 
-      disableBtn(currentCount,totalCards,nextButton)
       data.forEach((item, index) => {
         if (index === currentIndex) {
           item.classList.add("active");
-          console.log(item,index,'if')
         } else {
-           console.log(item,'else')
           item.classList.remove("active");
         }
-        
       })
-      container.style.transform = `translateX(-${currentIndex * 300}px)`;
+      
+      calcTransform(containerParticipants)
     }
   }
 
-  function moveCards(direction) {
-    const maxIndex = cards.length - 1;
-    if (direction === "next") {
-      currentIndex = (currentIndex === maxIndex) ? 0 : currentIndex + 1;
-    } else if (direction === "prev") {
-      currentIndex = (currentIndex === 0) ? maxIndex : currentIndex - 1;
+  function changeSlide(direction) {
+    if(direction ==='next'){ 
+      currentIndex++
+      if(currentIndex === totalCards)
+        {
+          currentIndex = 0
+        }
+    } else if(direction ==='prev'){
+      console.log(currentIndex)
+      currentIndex--
+      if(currentIndex < 0){
+        currentIndex = totalCards -1
+      }
     }
     updateUI(cards);
   }
 
-  prevButton.addEventListener("click", () => moveCards("prev"));
-  nextButton.addEventListener("click", () => moveCards("next"));
+  const calcTransform = (container) => {
+    const width = document.body.clientWidth;
+    if(width <= 485){
+      index = 19
+    } else{
+      index = 25
+    }
+    container.style.transform = `translateX(-${currentIndex  * index}%)`;
+  }
 
   window.addEventListener("resize", () => {
     updateUI(cards);
   });
+
+  prevButton.addEventListener("click", () => changeSlide("prev"));
+  nextButton.addEventListener("click", () => changeSlide("next"));
 
   updateUI(cards);
 });
